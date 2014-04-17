d3dd9be5e7f0c99487d5a706542d721d0af24a39
Jose R. Borja <chakaitos@gmail.com>
2014-04-17 17:44:27 -0500

    Implement binary search methods - all tests pass

--- bsearch.rb
+++ bsearch.rb
@@ -4,17 +4,38 @@ module ArrayUtil
   def self.bsearch(array, element)
     # If the array is empty return a result
     # Otherwise call your helper _bsearch method
+    start_index = 0
+    end_index = array.length-1
+    if array == []
+      return false
+    end
+    ArrayUtil._bsearch(array, element, start_index, end_index)
   end
 
   def self._bsearch(array, element, start_index, end_index)
     # If the start and end indeces are the same, search
     # that element and return
-    #
+
+    if array[start_index] == array[end_index] && array[end_index] == element
+      return true
+    elsif array[start_index] == array[end_index]
+      return false
+    end
+
     # Otherwise, find the midpoint
-    #   midpoint = start_index + (end_index - start_index) / 2
-    #
+    midpoint = start_index + (end_index - start_index) / 2
+
     # If the element is greater than the one at midpoint, call
     #   _bsearch on the right half of the remaining array
     # Otherwise, call _bsearch on left half of the remaining array
+    if array[midpoint] == element
+      return true
+    elsif array[midpoint] < element
+      end_index = midpoint
+      return _bsearch(array, element, start_index, end_index)
+    elsif array[midpoint] > element
+      start_index = midpoint + 1
+      return _bsearch(array, element, start_index, end_index)
+    end
   end
 end
